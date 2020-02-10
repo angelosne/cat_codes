8a87d16a99ea1eb73f9c92fdb694f68d9598299d
Angelosne <negkas.aggelos@gmail.com>
2020-02-10 13:13:37 +0200

    first commit

---
+++ app/src/main/java/com/symmetron/cat_codes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.symmetron.cat_codes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
